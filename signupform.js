e48f4004a56f46183078cd03e2e4afab3d0fb65c
126511 <126511@dgwcloud.nl>
2023-01-21 19:30:15 +0100

    tracking

--- signupform.js
+++ signupform.js
@@ -87,4 +87,62 @@ function validateZip(obj){
                                                             ?"none" : "block";
 }
 
-//doe zo hier die trakcing fzo
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Initialize variables
+    let clicks = 0;
+    let start = Date.now();
+    let keypresses = 0;
+    let characterstyped = 0;
+
+    // Add event listeners to track mouse clicks
+    document.addEventListener("click", function() {
+        clicks++;
+    });
+
+    // Add event lisenter to keypresses
+    document.addEventListener("keyup", function(event) {
+        keypresses++;
+    });
+
+    // Grab all input fields
+    let inputs = document.getElementsByTagName("input");
+
+    // Add an event listener to each input field 
+    for (let i = 0; i < inputs.length; i++) {
+
+        let previousValue = "";
+
+        inputs[i].addEventListener("keyup", function(event) {
+            // Find the current value of the field
+            let currentValue = event.target.value;
+
+            // Compute the new amount of characters typed in this field
+            characterstyped += currentValue.length - previousValue.length;
+
+            // Set previous value to correct value
+            previousValue = currentValue;
+        });
+    }
+
+    // Get the form and add a submit event listener
+    let form = document.getElementById("signupform");
+    form.addEventListener("submit", event => {
+
+        // Find end time and compute minutes and seconds (end is in ms)
+        let end = Date.now();
+        let minutes = Math.floor(((end - start)/ 1000) / 60);
+        let seconds = ((end - start) / 1000) % 60;
+
+        // Get the div and populate it with the data
+        let div = document.getElementById("tracking");
+        div.innerHTML = "Number of mouse clicks: " + clicks + "<br>" +
+                            "Total time spent: " + minutes + " minutes " + seconds + " seconds" + "<br>" +
+                            "Total key presses: " + keypresses + "<br>" +
+                            "Total number of characters typed: " + characterstyped;
+
+        // Show the div
+        div.style.display = "block";
+    });
+})
